86473064c0e2aecfa963989492ef636bd3e890b6
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-16 18:04:47 +0300

    task1 added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Task1
+        System.out.println("Task 1");
+        int daysInMonth = 30;
+        int costsPerDay[] = generateRandomArray(daysInMonth);
+        int sum = 0;
+        for (int i = 0; i < costsPerDay.length; i++) {
+            sum += costsPerDay[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+    }
+
+    public static int[] generateRandomArray(int days) {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[days];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
