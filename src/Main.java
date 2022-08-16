50e60d5de161240241a10d388f9aef9808b24f89
Andrew Meleshnikov <andreymeleshnikov@gmail.com>
2022-08-16 18:43:10 +0300

    task2 added

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,27 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
+        //Task2
+        System.out.println("\nTask 2");
+        int max = costsPerDay[0];
+        int min = costsPerDay[0];
+        int maxDay = 0;
+        int minDay = 0;
+
+        for (int i = 0; i < costsPerDay.length; i++) {
+            if (min > costsPerDay[i]) {
+                min = costsPerDay[i];
+                minDay = i + 1;
+            }
+            if (max < costsPerDay[i]) {
+                max = costsPerDay[i];
+                maxDay = i + 1;
+            }
+        }
+
+        System.out.print("Минимальная сумма трат за " + minDay + "-ое число составила " + min + " рублей. ");
+        System.out.println("Максимальная сумма трат за " + maxDay + "-ое число составила " + max + " рублей.");
+
     }
 
     public static int[] generateRandomArray(int days) {
